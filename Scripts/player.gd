4431ac709967f20da4798aa0abfa8060733cf1f5
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-02-16 23:00:14 +0200

    Update player.gd

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,9 +3,8 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	test()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func test() -> void:
+	print("hello form user created function")
