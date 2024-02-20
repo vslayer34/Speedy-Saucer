266187aa33af9041565773b09bb3d6d5bc263d83
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-02-21 00:47:35 +0200

    added apply_force in physics process

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,4 +3,14 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	apply_impulse(Vector2(0.0, -25.0))
+	#apply_impulse(Vector2(0.0, -25.0))
+	pass
+
+
+func _process(_delta: float) -> void:
+	#apply_force(Vector2(25.0, 0.0))
+	pass
+
+
+func _physics_process(_delta: float) -> void:
+	apply_force(Vector2(25.0, 0.0))
