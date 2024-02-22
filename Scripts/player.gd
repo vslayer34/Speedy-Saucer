84b8304fa603aeac667dbb010ba4dfab6bd2835a
vslayer34 <mohamd.elnagar.34@gmail.com>
2024-02-22 23:13:03 +0200

    Removed examples from player scripts and aded Camera 2D to the player
    Enabled smoothing and vertical and hotizontal drag for the camera2d

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,14 +2,6 @@ extends RigidBody2D
 
 var force_amount: float = 500.0
 
-func _ready() -> void:
-	print(test())
-	var example_value: int = test()
-	print("Example Value: " + str(example_value))
-	
-	print(add(5, 6))
-	var result: int = add(5, 7)
-	print("5 + 7 = " + str(result))
 
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("move_right"):
@@ -23,12 +15,3 @@ func _physics_process(_delta: float) -> void:
 	
 	if Input.is_action_pressed("move_down"):
 		apply_force(Vector2(0.0, force_amount))
-
-
-func test() -> int:
-	var returned_value: int = 10
-	return returned_value + 10
-
-
-func add(num1: int, num2: int) -> int:
-	return num1 + num2
